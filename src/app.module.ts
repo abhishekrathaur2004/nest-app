d403dca449d2d3e4ae7a4ceeebf488a2aacec6e9
Abhishek Krishnan Rathaur <abhishekkrishnanrathaur@Abhisheks-MacBook-Pro.local>
2024-06-27 15:15:39 +0530

    Added Middleware in the app using injectable annoter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { NoteMoudle } from './notes/notes.module';
+import { LoggerMiddleWare } from './middleware/logger.middleware';
 
 
 @Module({
@@ -7,8 +8,13 @@ import { NoteMoudle } from './notes/notes.module';
   controllers: [],
   providers: [],
 })
-export class AppModule {
+export class AppModule implements NestModule {
   constructor(){
     console.log('app module started')
+  }
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleWare)
+      .forRoutes('note');
   }
 }
